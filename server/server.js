336af011676d0639eecdcd986e13fd811163d774
Renata Maraj <renatavmaraj@gmail.com>
2017-01-27 12:07:54 -0800

    Server up and running

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ var app = express();
 var port = process.env.PORT || 3021;
 
 
-app.engine('html', require('ejs').renderFile);
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../public'));
 
-require('./config/routes.js')(app, express);
+app.use(express.static(__dirname + '/../client'));
+
+//require('./config/routes.js')(app, express);
 
 app.listen(port, function() {
       console.log('Listening on ' + port);
