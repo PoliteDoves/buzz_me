d31dc2d514fc949e362900fde1ba6b9cfcbb9c70
Jimmie Gisclair <jimmiegisclair@gmail.com>
2017-01-27 11:28:37 -0800

    Setup/open connection to mySQL/Sequelize database

--- database/database.js
+++ database/database.js
@@ -1,1 +1,13 @@
 var Sequelize = require('sequelize');
+
+db = new Sequelize('database_name', 'root', '', {
+  dialect: 'mysql',
+  port: 3306
+});
+
+db.authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  }, function(err) {
+    console.log('Unable to connect to the database: ', err);
+  });
