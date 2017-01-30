cc7a4fcbeaaa8ddb0b25d6ad7800bf84b1f0d359
Jimmie Gisclair <jimmiegisclair@gmail.com>
2017-01-30 14:39:15 -0800

    Import schemas into the server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser= require('body-parser');
 var path= require('path');
 var morgan = require('morgan');
+var db = require('../database/schemas.js');
 var app = express();
 var port = process.env.PORT || 3021;
 
@@ -29,8 +30,8 @@ app.use(express.static(__dirname + '/../node_modules'));
 //require('./config/routes.js')(app, express);
 
 app.listen(port, function() {
-      console.log('Listening on ' + port);
-  });
+  console.log('Listening on ' + port);
+});
 
 module.exports = {
   app: app,
