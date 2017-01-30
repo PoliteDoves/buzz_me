a7e7cfd5da312f76f324fdf046f6668eb2cc13a2
apslagle <apslagle@gmail.com>
2017-01-30 15:39:55 -0600

    routing spec and suggested tests

--- spec/client/clientSpec.js
+++ spec/client/clientSpec.js
@@ -11,13 +11,19 @@ describe('Routing', function () {
   it('Should have /login route, template, and controller', function () {
     expect($route.routes['/login']).to.be.defined;
     expect($route.routes['/login'].controller).to.equal('LoginController');
-    expect($route.routes['/login'].templateUrl).to.equal(''); //TODO
+    expect($route.routes['/login'].templateUrl).to.equal('app/login/login.html'); //TODO
   });
 
   it('Should have /home route, template, and controller', function () {
     expect($route.routes['/home']).to.be.defined;
     expect($route.routes['/home'].controller).to.equal('LandingController');
-    expect($route.routes['/home'].templateUrl).to.equal(''); //TODO
+    expect($route.routes['/home'].templateUrl).to.equal('app/landing/landing.html'); //TODO
+  });
+
+  it('Should have /list route, template, and controller', function () {
+    expect($route.routes['/list']).to.be.defined;
+    expect($route.routes['/list'].controller).to.equal('ListController');
+    expect($route.routes['/list'].templateUrl).to.equal('app/list/list.html'); //TODO
   });
 
 });
