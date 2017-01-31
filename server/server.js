02bc462fdb1e4c91453d9784ba8c75efdf0f633b
maxuuell <maxuuell@gmail.com>
2017-01-31 10:38:51 -0800

    Added login link to authO on landing page. Updated routes to landing/home page.

--- server/server.js
+++ server/server.js
@@ -6,20 +6,6 @@ var db = require('../database/schemas.js');
 var app = express();
 var port = process.env.PORT || 3021;
 
-var allowCrossDomain = function(req, res, next) {
-    if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
-
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
