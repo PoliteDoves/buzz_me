d3d776dff561f5ed56a2a620335045a786d1adf8
Jimmie Gisclair <jimmiegisclair@att.net>
2017-01-31 14:12:28 -0800

    Add ability to post a new user to the database

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,16 @@
 
 
-// module.exports = function(app, express) {
-//   app.get('/api/', )//add routes and controller based on database endpoints
-// }
+module.exports = function(app, express, db) {
+  //add routes and controller based on database endpoints
+
+  app.post('/api/users', function(req, res) {
+    console.log('Got your POST request');
+    console.log('Reqest body: ', req.body);
+    db.Users.create({
+      name: req.body.name,
+      phone_number: req.body.phoneNumber,
+      email: req.body.email
+    });
+    res.send('Success');
+  });
+}
