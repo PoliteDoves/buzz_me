3c7dbfd1e64ab949d4e270c9c7299805c3c5d19c
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-01 09:31:57 -0800

    Only require morgan in development

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var bodyParser= require('body-parser');
 var path= require('path');
-var morgan = require('morgan');
 var db = require('../database/schemas.js');
 var app = express();
 var port = process.env.PORT || 3021;
 
 if(!process.env.PORT) {
+  var morgan = require('morgan');
   app.use(morgan('dev'));
 }
 
