2b9c91f6ae1dcf4ce7effdc6af4e52648b2bb541
apslagle <apslagle@gmail.com>
2017-02-02 12:43:48 -0600

    spec attempt: spec not functional

--- spec/client/clientSpec.js
+++ spec/client/clientSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Routing', function () {
+xdescribe('Routing', function () {
   var $route;
   beforeEach(module('app'));
 
@@ -25,5 +25,59 @@ describe('Routing', function () {
     expect($route.routes['/list'].controller).to.equal('ListController');
     expect($route.routes['/list'].templateUrl).to.equal('app/list/list.html'); //TODO
   });
+});
 
+xdescribe('ListController', function () {
+  var $scope, $rootScope, createController, ListFactory, $httpBackend;
+
+  // using angular mocks, we can inject the injector
+  // to retrieve our dependencies
+  beforeEach(module('app'));
+  beforeEach(inject(function ($injector) {
+
+    // mock out our dependencies
+    $rootScope = $injector.get('$rootScope');
+    $httpBackend = $injector.get('$httpBackend');
+    ListFactory = $injector.get('ListFactory');
+    $scope = $rootScope.$new();
+
+    var $controller = $injector.get('$controller');
+
+    var profile = {name: "Bob", email: "apslagle@gmail.com"};
+    vm.payload = profile;
+
+    createController = function () {
+      return $controller('ListController', {
+        $scope: $scope,
+        ListFactory: ListFactory
+      });
+    };
+
+  }));
+
+  it('should have a vm', function () {
+    createController();
+    expect(vm).to.be.an('object');
+  });
+
+  it('should call `ListFactory.getUserTasks` when controller is loaded', function () {
+    sinon.spy(ListFactory, 'getUserTasks');
+    $httpBackend.expectGET('/api/tasks/apslagle@gmail.com').respond(200);
+
+    createController();
+    $httpBackend.flush();
+
+    expect(ListFactory.getUserTasks.called).to.equal(true);
+    ListFactory.getUserTasks.restore();
+  });
+
+  it('should populate the data property after the call to `Links.getAll()`', function () {
+    var mockLinks = [{}, {}, {}];
+    $httpBackend.expectGET('api/tasks/apslagle@gmail.com').respond(mockLinks);
+
+    createController();
+    $httpBackend.flush();
+
+    expect(vm.tasks).to.deep.equal(mockLinks);
+  });
 });
