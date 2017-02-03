cf7b7cc149ce7365e1b82c1edd5383e31973e162
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-03 10:56:40 -0800

    add stylesheet and add jquery to make button close modal

--- client/app/list/listController.js
+++ client/app/list/listController.js
@@ -11,17 +11,18 @@
     vm.date = '';
     vm.time = '';
     vm.interval = 0;
+    vm.id;
 
     lock.getProfile(store.get('jwt'), function (error, profile) {
       vm.payload = profile;
       ListFactory.findOrCreateUser(profile.name, profile.email)
-      .then(user => {
-        ListFactory.getUserTasks(user.data[0].email)
-        .then(tasks=>{
-          vm.tasks = tasks
-        })
-        .catch(e=>console.log('error', e))
-      });
+        .then(user => {
+          ListFactory.getUserTasks(user.data[0].email)
+            .then(tasks=>{
+              vm.tasks = tasks
+            })
+            .catch(e=>console.log('error', e))
+        });
     });
 
     vm.authService = authService;
@@ -41,6 +42,18 @@
       if(num === 60) { return "Every Hour"; }
       if(num === 120) { return "Every 2 Hours"; }
       return `Every ${num} minutes`;
+    }
+
+    vm.onTextSubmit = function() {
+      var data = {};
+      // get date, time, interval from models
+      console.log('date', vm.date, 'time', vm.time, 'interval', vm.interval)
+      // convert date and time to utc
+      // make http request with vm.id
+    }
+
+    vm.setTaskId = function(id) {
+      vm.id = id;
     }
   }
 }());
