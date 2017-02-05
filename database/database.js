b4df4549696038e69f5d8d562dd660dee71152b3
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-05 13:47:12 -0800

    Add min pool size to try to correct error in production

--- database/database.js
+++ database/database.js
@@ -1,11 +1,22 @@
 var Sequelize = require('sequelize');
 
 if(process.env.DATABASE_URL) {
-  db = new Sequelize(process.env.DATABASE_URL);
+  db = new Sequelize(process.env.DATABASE_URL, {
+    pool: {
+      min: 1,
+      max: 5,
+      idle: 20000
+    }
+  });
 } else {
   db = new Sequelize('buzzme_db', 'root', '', {
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
+    pool: {
+      min: 1,
+      max: 5,
+      idle: 20000
+    }
   });
 }
 
